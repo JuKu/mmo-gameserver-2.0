956edbb62af04999f857658b94eb1b468a21bfff
juku <kuenzel.justin@t-online.de>
2018-11-22 17:44:07 +0100

    added logger configuration file.

--- server/src/main/java/com/jukusoft/mmo/gs/frontend/ServerMain.java
+++ server/src/main/java/com/jukusoft/mmo/gs/frontend/ServerMain.java
@@ -1,9 +1,37 @@
 package com.jukusoft.mmo.gs.frontend;
 
+import com.jukusoft.mmo.engine.shared.config.Config;
+import com.jukusoft.mmo.engine.shared.logger.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ServerMain {
 
     public static void main (String[] args) {
-        //
+        try {
+            start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    protected static void start () throws Exception {
+        //load logger config
+        try {
+            Config.load(new File("./config/logger.cfg"));
+
+            //initialize logger
+            Log.init();
+        } catch (IOException e) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, "Couldn't initialize config and logger!", e);
+            System.exit(0);
+        }
+
+        Log.i("Startup", "Started Proxy Server.");
     }
 
 }
