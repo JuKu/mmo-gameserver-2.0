555ed001a7f94d91bfeb6533089497803e55bad2
juku <kuenzel.justin@t-online.de>
2018-11-24 18:54:11 +0100

    added some junit tests for class ConfigLoader.

--- server/src/test/java/com/jukusoft/mmo/gs/frontend/utils/ConfigLoaderTest.java
+++ server/src/test/java/com/jukusoft/mmo/gs/frontend/utils/ConfigLoaderTest.java
@@ -1,8 +1,11 @@
 package com.jukusoft.mmo.gs.frontend.utils;
 
+import com.jukusoft.mmo.engine.shared.config.Config;
 import org.junit.Test;
 
 import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class ConfigLoaderTest {
 
@@ -14,6 +17,18 @@ public class ConfigLoaderTest {
     @Test
     public void testLoad () throws IOException {
         ConfigLoader.load("../config/", new String[0]);
+    }
+
+    @Test
+    public void testLoadWithParams () throws IOException {
+        ConfigLoader.load("../config/", new String[]{
+                "-test",
+                "-Config:section1.key2=value3"
+        });
+
+        assertEquals("value3", Config.get("section1", "key2"));
+
+        Config.clear();
     }
 
 }
