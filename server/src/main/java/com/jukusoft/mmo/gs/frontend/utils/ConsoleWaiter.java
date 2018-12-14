b4cc4b3ae2b3676e9b778cdb78dd6f40328ed9f8
juku <kuenzel.justin@t-online.de>
2018-12-15 00:38:36 +0100

    changed class ConsoleWaiter, added support to create region on ftp server.

--- server/src/main/java/com/jukusoft/mmo/gs/frontend/utils/ConsoleWaiter.java
+++ server/src/main/java/com/jukusoft/mmo/gs/frontend/utils/ConsoleWaiter.java
@@ -1,13 +1,21 @@
 package com.jukusoft.mmo.gs.frontend.utils;
 
+import com.jukusoft.mmo.engine.shared.config.Config;
 import com.jukusoft.mmo.engine.shared.logger.Log;
 import com.jukusoft.mmo.engine.shared.utils.Utils;
+import com.jukusoft.mmo.gs.region.ftp.FTPFactory;
+import io.github.bckfnn.ftp.FtpClient;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.CountDownLatch;
 
 public class ConsoleWaiter {
+
+    protected static final String LOG_TAG = "CLI";
 
     protected ConsoleWaiter () {
         //
@@ -22,7 +30,7 @@ public class ConsoleWaiter {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Log.i("CLI", "command line input is accepted yet. Quit server with 'quit' and ENTER.");
+        Log.i(LOG_TAG, "command line input is accepted yet. Quit server with 'quit' and ENTER.");
 
         while (!Thread.interrupted()) {
             //read line
@@ -30,6 +38,37 @@ public class ConsoleWaiter {
 
             if (line.equals("quit") || line.equals("exit")) {
                 break;
+            } else if (line.startsWith("/createFTPRegion")) {
+                String[] args = line.split(" ");
+
+                if (args.length < 2) {
+                    Log.w(LOG_TAG, "region id is required! Correct usage: /createFTPRegion <ID>");
+                    continue;
+                }
+
+                long regionID = Long.parseLong(args[1]);
+                Log.i(LOG_TAG, "try to create region " + regionID + "...");
+
+                //open ftp connection
+                FtpClient ftp = FTPFactory.createSync();
+
+                CountDownLatch latch = new CountDownLatch(1);
+
+                ftp.mkd(Config.get("FTP", "regionsDir") + "/region_" + regionID + "_1", res -> {
+                    if (res.succeeded()) {
+                        Log.i(LOG_TAG, "created region with regionID " + regionID + " on ftp server successfully!");
+                    } else {
+                        Log.w(LOG_TAG, "Couldn't create region with regionID " + regionID + " on ftp server!", res.cause());
+                    }
+
+                    latch.countDown();
+                });
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    Log.w(LOG_TAG, "InterruptedException: ", e);
+                }
             }
 
             System.out.println("Unsupported command: " + line);
