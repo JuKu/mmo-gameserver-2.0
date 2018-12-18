9ad1b349799b002ae9eeb65ce09c3d05d5fce905
juku <kuenzel.justin@t-online.de>
2018-12-18 18:11:42 +0100

    added some junit tests for class DummyHandler.

--- region-api/src/test/java/com/jukusoft/mmo/gs/region/database/DatabaseUpgraderTest.java
+++ region-api/src/test/java/com/jukusoft/mmo/gs/region/database/DatabaseUpgraderTest.java
@@ -168,7 +168,6 @@ public class DatabaseUpgraderTest {
 
         DatabaseUpgrader databaseUpgrader = new DatabaseUpgrader(mySQLConfig);
         databaseUpgrader.migrate();
-
     }
 
 }
---
+++ region-api/src/test/java/com/jukusoft/mmo/gs/region/utils/DummyHandlerTest.java
@@ -0,0 +1,13 @@
+package com.jukusoft.mmo.gs.region.utils;
+
+import org.junit.Test;
+
+public class DummyHandlerTest {
+
+    @Test
+    public void testHandle () {
+        DummyHandler handler = new DummyHandler();
+        handler.handle(null);
+    }
+
+}
