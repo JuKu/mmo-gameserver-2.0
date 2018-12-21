e0853f955f571b7dea7625cd6caf40ee4b31329b
juku <kuenzel.justin@t-online.de>
2018-12-21 15:03:13 +0100

    added some junit tests for class ClientInitializer.

--- server/src/test/java/com/jukusoft/mmo/gs/frontend/network/ClientInitializerTest.java
+++ server/src/test/java/com/jukusoft/mmo/gs/frontend/network/ClientInitializerTest.java
@@ -3,6 +3,7 @@ package com.jukusoft.mmo.gs.frontend.network;
 import com.jukusoft.mmo.gs.region.RegionManager;
 import com.jukusoft.vertx.connection.clientserver.RemoteConnection;
 import com.jukusoft.vertx.connection.stream.BufferStream;
+import io.netty.util.collection.LongObjectMap;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -17,6 +18,10 @@ public class ClientInitializerTest {
     public void testHandleConnect () {
         ClientInitializer clientInitializer = new ClientInitializer(Mockito.mock(RegionManager.class));
         clientInitializer.handleConnect(Mockito.mock(BufferStream.class), Mockito.mock(RemoteConnection.class));
+
+        for (LongObjectMap.PrimitiveEntry<ClientHandler> entry : clientInitializer.connections.entries()) {
+            entry.value().onClose(Mockito.mock(RemoteConnection.class));
+        }
     }
 
 }
