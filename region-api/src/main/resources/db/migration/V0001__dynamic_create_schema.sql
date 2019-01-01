e59606e98e0def3591dda94f93eeea86a66ca1cf
juku <kuenzel.justin@t-online.de>
2019-01-01 16:26:42 +0100

    added mysql table global_settings.

--- region-api/src/main/resources/db/migration/V0001__dynamic_create_schema.sql
+++ region-api/src/main/resources/db/migration/V0001__dynamic_create_schema.sql
@@ -9,3 +9,13 @@ CREATE TABLE `${prefix}character_positions` (
  `visible` int(10) NOT NULL DEFAULT '1',
  PRIMARY KEY (`cid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `${prefix}global_settings` (
+ `area` varchar(64) NOT NULL,
+ `key` varchar(64) NOT NULL,
+ `value` varchar(255) NOT NULL,
+ `load_on_init` int(10) NOT NULL DEFAULT '1',
+ PRIMARY KEY (`area`,`key`),
+ KEY `ix_area` (`area`),
+ KEY `ix_load_on_init` (`load_on_init`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
